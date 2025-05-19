8c489e428f1831b5f4bd2ab24ad62f40463124d2
Ronaldo Ruiz <rrfilho2@hotmail.com>
2025-05-19 18:53:32 -0300

    - pequenos ajustes;

--- libs/led_matrix.c
+++ libs/led_matrix.c
@@ -5,11 +5,7 @@
 #include <stdio.h>
 
 #define PIXELS 25
-#define COLOR 6
-#define FIRST_BYTE 24
-#define SECOND_BYTE 16
-#define THIRD_BYTE 8
-#define LED_PIN 7
+#define LED_MATRIX_PIN 7
 #define ON 180430
 #define OFF 0
 #define BRIGHTNESS_FACTOR 0.01
@@ -21,7 +17,7 @@ void led_matrix_init() {
     set_sys_clock_khz(128000, false);
     unsigned int offset = pio_add_program(PIO_ID, &pio_matrix_program);
     sm = pio_claim_unused_sm(PIO_ID, true);
-    pio_matrix_program_init(PIO_ID, sm, offset, LED_PIN);
+    pio_matrix_program_init(PIO_ID, sm, offset, LED_MATRIX_PIN);
 }
 
 void led_matrix_draw(unsigned int level) {
